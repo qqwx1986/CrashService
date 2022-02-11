b2b791107a549fb5527c16337076c8d0e037166f
wangxu <399530787@qq.com>
2022-02-11 15:23:59 +0800

    删除多余日志

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"CrashService/client"
 	"CrashService/server"
-	"github.com/sirupsen/logrus"
 	"os"
 	"strings"
 )
 
 func main() {
-	for i, arg := range os.Args {
-		logrus.Infof("%d %s", i, arg)
-	}
 	if len(os.Args) < 2 {
 		return
 	}
